243ef971652e5a66274154da0cb727a7c78d41a2
Devon Deason <devon.d.deason@gmail.com>
2020-05-25 07:39:10 -0600

    add a bit of error handling

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,9 +6,10 @@ import SEO from "../components/seo";
 import Sidebar from "./../components/IndexPage/Sidebar";
 
 function IndexPage() {
-  const [error, setError] = useState();
-  const [location, setLocation] = useState("");
-  const [weatherData, setWeatherdata] = useState({});
+  const [error, setError] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [location, setLocation] = useState(null);
+  const [weatherData, setWeatherdata] = useState(null);
 
   useEffect(() => {
     const getData = async () => {
@@ -17,40 +18,59 @@ function IndexPage() {
         return;
       }
 
+      setLoading(true);
+
       const success = async (position) => {
-        const openCageResult = await axios.get(
-          `./.netlify/functions/getLocation`,
-          {
-            params: {
-              lat: position.coords.latitude,
-              lng: position.coords.longitude,
-            },
-          }
-        );
-        console.log("success -> openCageResult", openCageResult);
+        // openCage is used for out reverse geoCoding features
+        try {
+          const openCageResult = await axios.get(
+            `./.netlify/functions/getLocation`,
+            {
+              params: {
+                lat: position.coords.latitude,
+                lng: position.coords.longitude,
+              },
+            }
+          );
 
-        const openWeatherResult = await axios.get(
-          `./.netlify/functions/getWeather`,
-          {
-            params: {
-              lat: position.coords.latitude,
-              lng: position.coords.longitude,
-            },
-          }
-        );
+          const locationText = openCageResult.data.city
+            ? `${openCageResult.data.city}, ${openCageResult.data.state}`
+            : `${openCageResult.data.county}, ${openCageResult.data.state}`;
 
-        setWeatherdata(openWeatherResult.data);
+          setLocation(locationText);
 
-        const locationText = openCageResult.data.city
-          ? `${openCageResult.data.city}, ${openCageResult.data.state}`
-          : `${openCageResult.data.county}, ${openCageResult.data.state}`;
+          console.log("success -> openCageResult", openCageResult);
+        } catch (e) {
+          setLoading(false);
+          setError(e.message);
+          console.log("openCageResult error: ", e.message);
+          return;
+        }
 
-        console.log("success -> locationText", locationText);
-
-        setLocation(locationText);
+        // Now let's go get our weather data
+        try {
+          const openWeatherResult = await axios.get(
+            `./.netlify/functions/getWeather`,
+            {
+              params: {
+                lat: position.coords.latitude,
+                lng: position.coords.longitude,
+              },
+            }
+          );
+          setWeatherdata(openWeatherResult.data);
+        } catch (e) {
+          setLoading(false);
+          setError(e.message);
+          console.log("openWeatherResult: ", e.message);
+          return;
+        } finally {
+          setLoading(false);
+        }
       };
 
       const reject = (error) => {
+        setLoading(false);
         setError(error.message);
         return;
       };
@@ -60,20 +80,26 @@ function IndexPage() {
 
     getData();
   }, []);
+
   console.log("weatherData", weatherData);
   console.log(location);
+
   return (
     <Layout>
       <SEO
         keywords={[`gatsby`, `tailwind`, `react`, `tailwindcss`]}
         title="Home"
       />
-      {error ? (
-        error
-      ) : (
+      {error && <div>{error}</div>}
+      {loading && (
+        <div>
+          <h2>Loading...</h2>
+        </div>
+      )}
+      {location && weatherData && (
         <div className="flex flex-grow flex-col md:flex-row">
           <section className="bg-blue-500 md:w-1/5">
-            <Sidebar location={location} />
+            <Sidebar location={location} data={weatherData.current} />
           </section>
           <section className="bg-yellow-500 flex-1 md:w-4/5">Content</section>
         </div>
