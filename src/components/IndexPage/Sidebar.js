371ce43fe2e75f7728829dbbd3e5e9efac2c0b3e
Devon Deason <devon.d.deason@gmail.com>
2020-05-25 08:24:17 -0600

    sidebar init

--- src/components/IndexPage/Sidebar.js
+++ src/components/IndexPage/Sidebar.js
@@ -2,9 +2,23 @@ import React from "react";
 import { PropTypes } from "prop-types";
 
 const Sidebar = ({ location, data }) => {
-  console.log("Sidebar -> data", data);
-  console.log("Sidebar -> location", location);
-  return <div>{location}</div>;
+  return (
+    <div>
+      <div className="text-center">
+        <h2 className=" text-3xl">{location}</h2>
+        <div className="flex justify-center items-center">
+          <img
+            src={`http://openweathermap.org/img/w/${data.weather[0].icon}.png`}
+          />
+          <h3 className="text-5xl mx-2">{Math.floor(data.temp)}</h3>
+          <div className="flex flex-col">
+            <span>F</span>
+            <span>C</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 Sidebar.propTypes = {
